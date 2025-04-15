aa8318b600c7d42033b6c679ced4ae4f18464cf8
Mitchell de Vries <mitchelldevries2001@gmail.com>
2025-04-16 00:07:54 +0200

    process schanfge

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: { ignoreDuringBuilds: true },
+    env: {
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+      },
 
 };
 
