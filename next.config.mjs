3b87c4911923ae56fb2d15cd931a5690136b5600
Mitchell de Vries <mitchelldevries2001@gmail.com>
2025-04-21 19:17:48 +0200

    config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: { ignoreDuringBuilds: true },
-    env: {
-        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-      },
+const isProd = process.env.NODE_ENV === 'production';
 
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+  assetPrefix: isProd ? '/' : '',
 };
 
 export default nextConfig;
